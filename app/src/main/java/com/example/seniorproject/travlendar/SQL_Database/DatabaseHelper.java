76bf47b3c6d01914fb340afa0d92eb69f61eb8f0
jiduncan <jiduncan2724@gmail.com>
2018-12-11 22:46:20 -0600

    Update DatabaseHelper.java

--- app/src/main/java/com/example/seniorproject/travlendar/SQL_Database/DatabaseHelper.java
+++ app/src/main/java/com/example/seniorproject/travlendar/SQL_Database/DatabaseHelper.java
@@ -3,13 +3,13 @@ package com.example.seniorproject.travlendar.SQL_Database;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import com.example.seniorproject.travlendar.modal.User;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.sql.Types.NULL;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     // Database Version
@@ -40,14 +40,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
    @Override
     public void onCreate(SQLiteDatabase db) {
-       db.execSQL(DROP_USER_TABLE);
-       onCreate(db);
+       db.execSQL(CREATE_USER_TABLE);
+       //onCreate(db);
    }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(DROP_USER_TABLE);
-
         onCreate(db);
     }
 
